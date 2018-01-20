9a4f88004d677dd3f8529d21b2b7bbaae3b2c450
Grisha <hersheleh@gmail.com>
2018-01-20 11:14:53 -0800

    textbooks display in a table on texbook app index page

--- textbook/admin.py
+++ textbook/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+from .models import Textbook
+
+admin.site.register(Textbook)
+
 # Register your models here.
--- textbook/views.py
+++ textbook/views.py
@@ -3,7 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Textbook
+
 def index(request):
-    return render(request, 'textbook/index.html')
+    textbooks = Textbook.objects.all()
+    return render(request, 'textbook/index.html',{"textbooks" : textbooks })
 
 # Create your views here.
